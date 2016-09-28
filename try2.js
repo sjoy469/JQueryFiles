9e30c0d323d717e3ae53476e7aa4770b3be4287b
sjoy469 <sjoy469@aucklanduni.ac.nz>
2016-09-28 13:24:21 +1300

    Add files via upload

---
+++ try2.js
@@ -0,0 +1,109 @@
+
+<!DOCTYPE HTML>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="edit-Type" edit="text/html; charset=utf-8">
+
+    
+      <meta name="robots" content="index, follow">
+      <meta name="googlebot" content="index, follow">
+
+      <script type="text/javascript" src="/js/embed/highlight.pack.js?update_16_09_2016_1"></script>
+
+      <script type="text/javascript" src="/js/embed/embed.js?update_16_09_2016_1"></script>
+
+
+      
+        <link rel="stylesheet" type="text/css" href="/css/embed/embed-light.css?update_16_09_2016_1"/>
+
+      
+
+      <script type="text/javascript">
+        var height;
+        var force_height = null;
+        var slug = "817w0tq2";
+        var show_src = "//fiddle.jshell.net/sjoy469/817w0tq2/show/light/";
+        var resize_element_counter = 0;
+        var shell_edit_url = '/sjoy469/817w0tq2/light/';
+
+        
+      </script>
+    
+
+    <link href="//fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+    <!-- <link href="//fonts.googleapis.com/css?family=Lato:400|Inconsolata" rel="stylesheet" type="text/css"> -->
+
+    <style type="text/css" media="screen">
+      
+      
+      
+      
+    </style>
+
+  </head>
+  <body>
+    <div id="wrapper">
+      
+        <header>
+          <h1><a href="/sjoy469/817w0tq2/?utm_source=website&amp;utm_medium=embed&amp;utm_campaign=817w0tq2" target="_blank">Edit in JSFiddle</a></h1>
+          <!-- <h1><a href="/sjoy469/817w0tq2/light/" target="_blank">Edit in JSFiddle</a></h1> -->
+          <div id="actions">
+            <ul class="normalRes">
+              
+                <li class="active">
+                  <a data-trigger-type="result" href="#Result">Result</a>
+                </li>
+              
+            </ul>
+            <div class="hl"></div>
+          </div>
+        </header>
+
+        <div id="tabs">
+          
+            <div class="tCont result active" id="result"></div>
+              
+                <script type="text/javascript">
+                  window.addEventListener('load', function(){
+                    if (typeof(EmbedManager) === undefined){
+                      
+                        EmbedManager.loadResult();
+                      
+                    }
+                  }, false);
+                </script>
+              
+            
+          
+        </div>
+      
+    </div>
+
+    
+      <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  var GA_LOCAL_STORAGE_KEY = 'ga:clientId';
+  if (window.localStorage) {
+    ga('create', 'UA-366077-19', {
+      'storage': 'none',
+      'clientId': localStorage.getItem(GA_LOCAL_STORAGE_KEY)
+    });
+    ga(function(tracker) {
+      localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get('clientId'));
+    });
+    ga('send', 'pageview', '/[user]/[fiddle]/embedded/[tabs]/'); // where `url_structure` is for example `/[user]/[fiddle]/[revision]/embedded/js,css/`
+  }
+</script>
+
+      
+    
+
+  </body>
+</html>
